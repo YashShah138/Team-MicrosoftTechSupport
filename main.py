cec554af9e3423fd76a5e1cb372017bf03e11a91
YashShah138 <89223735+YashShah138@users.noreply.github.com>
2021-11-17 11:52:59 -0800

    Added all base home pages, added tides api, updated readme and navbar.html. Added a static folder for all our images for the project.

--- main.py
+++ main.py
@@ -13,6 +13,30 @@ def index():
 def aboutus():
     return render_template("/assignments/aboutus.html")
 
+@app.route('/surfingData/')
+def surfingData():
+    return render_template("surfingData.html")
+
+@app.route('/yash/')
+def yash():
+    return render_template("/assignments/AboutUs/yash.html")
+
+@app.route('/akhil/')
+def akhil():
+    return render_template("/assignments/AboutUs/akhil.html")
+
+@app.route('/valen/')
+def valen():
+    return render_template("/assignments/AboutUs/valen.html")
+
+@app.route('/avinh/')
+def avinh():
+    return render_template("/assignments/AboutUs/avinh.html")
+
+@app.route('/jay/')
+def jay():
+    return render_template("/assignments/AboutUs/jay.html")
+
 # run page lol
 if __name__ == "__main__":
     app.run(debug=True)
