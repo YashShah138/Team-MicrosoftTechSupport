07bfdb5d08e160b82f20f6fae1ea680ba7033e89
valenreynolds <valenryanreynolds@gmail.com>
2022-01-05 21:13:36 -0800

    Survey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from __init__ import app
 from flask import render_template, request
+from flask import request, redirect
 import requests
 
 from crud.app_crud import app_crud
@@ -33,6 +34,36 @@ def BeachLocation1():
 @app.route('/beachlocation2/')
 def BeachLocation2():
     return render_template("/assignments/BeachLocation2.html")
+
+def selectBestBeach(form):
+    return "DelMar is the best beach for you"
+
+@app.route('/favoritebeach1/', methods=["GET", "POST"])
+def FavoriteBeachSurvey():
+    feedback = "Beach Not Identified"
+    SolanaBeach = 0
+    DelMar = 0
+    if request.method == "POST":
+        if (request.form.get("surfing") != None):
+            SolanaBeach += 1
+        if (request.form.get("volleyball") != None):
+            DelMar += 1
+        if (request.form.get("dog") != None):
+            DelMar += 1
+        if (request.form.get("crowd") != None):
+            SolanaBeach += 1
+        if (request.form.get("picnic") != None):
+            DelMar += 1
+        if (DelMar == 0 and SolanaBeach == 0):
+            feedback = "Pick at least one preference"
+        elif (DelMar > SolanaBeach):
+            feedback = "Del Mar beach is best for you"
+        elif (SolanaBeach > DelMar):
+            feedback = "Solana Beach is best for you"
+        elif (SolanaBeach == DelMar):
+            feedback = "Solana and Del Mar beach are good for you"
+
+    return render_template("/assignments/Survey.html", beach = feedback)
 
 @app.route('/yash/')
 def yash():
