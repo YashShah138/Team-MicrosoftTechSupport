d84fbf0d48e84c399c9899bbc468c67b51405234
akhil <akhil@nandhakumar.net>
2022-01-18 12:25:01 -0800

    gitignore

--- main.py
+++ main.py
@@ -77,9 +77,9 @@ def yash():
     url = "https://yourf1.p.rapidapi.com/drivers"
 
     headers = {
-    'x-rapidapi-host': "yourf1.p.rapidapi.com",
-    'x-rapidapi-key': "80e73128e0mshda8c95123266391p176951jsnbc06ff234f92"
-}
+        'x-rapidapi-host': "yourf1.p.rapidapi.com",
+        'x-rapidapi-key': "80e73128e0mshda8c95123266391p176951jsnbc06ff234f92"
+    }
 
     response = requests.request("GET", url, headers=headers)
     data = response.json()
@@ -102,8 +102,8 @@ def akhil():
     url = "https://sportscore1.p.rapidapi.com/tennis-rankings/atp"
     querystring = {"page":"1"}
     headers = {
-    'x-rapidapi-host': "sportscore1.p.rapidapi.com",
-    'x-rapidapi-key': "af654d789amshce4b35d071f3bd2p1c0cc8jsn8db3aa6a8acc"
+        'x-rapidapi-host': "sportscore1.p.rapidapi.com",
+        'x-rapidapi-key': "af654d789amshce4b35d071f3bd2p1c0cc8jsn8db3aa6a8acc"
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
@@ -119,8 +119,8 @@ def valen():
     url = "https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/win-stats/2021"
 
     headers = {
-    'x-rapidapi-host': "nfl-team-stats.p.rapidapi.com",
-    'x-rapidapi-key': "dabdc2fe99mshcfa1ae8827f4f16p1f550djsn5f1b6441c796"
+        'x-rapidapi-host': "nfl-team-stats.p.rapidapi.com",
+        'x-rapidapi-key': "dabdc2fe99mshcfa1ae8827f4f16p1f550djsn5f1b6441c796"
     }
     response = requests.request("GET", url, headers=headers)
     chargers=response.json()
