fc788ec5546560ad380e9354f3e185351f3e3575
akhil <akhil@nandhakumar.net>
2022-01-25 12:01:28 -0800

    changed mainpy and model py files for async

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from flask import request, redirect
 import requests
 
 from crud.app_crud import app_crud
+from crud.app_crud_api import app_crud_api
 app.register_blueprint(app_crud)
+app.register_blueprint(app_crud_api)
 
 # app.routes
 
