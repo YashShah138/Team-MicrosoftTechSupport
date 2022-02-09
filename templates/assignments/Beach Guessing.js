9809c652317b08a1f20ea5fddc39f4cd043be155
valenreynolds <valenryanreynolds@gmail.com>
2022-02-09 12:12:04 -0800

    Beach Guessing Progress.

---
+++ templates/assignments/Beach Guessing.js
@@ -0,0 +1,17 @@
+
+let ccdisplay = document.querySelector('.crrDisplay');
+let incdisplay = document.querySelector('.incDisplay');
+let guess = document.querySelector('#character');
+let textForm = document.querySelector('.textForm');
+
+var commonWords = [
+    "Surfing", "Beach Volleyball", "Dogs", "Sun Bathing", "Picnic", "Bonfire", "Running", "Walking", "Tanning", "Sandcastle", "Relaxing", "Snorkeling"
+var chooseRandomWord = function(array) {
+    return array[Math.floor(Math.random() * array.length)];
+
+    var counter = 10;
+    var triedCharacters = [];
+    var correctCharacters = [];
+
+    for (choenWord.length)
+}
