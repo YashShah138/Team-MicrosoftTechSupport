092286bfa23db79cb2e9f69921ed7a829b8532c5
avinh <avinhahuynh@gmail.com>
2022-02-16 10:54:47 -0800

    crud tghingd

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from __init__ import app
+from app import app
 from flask import render_template, request
 from flask import request, redirect
 import requests
 
-from crud.app_crud import app_crud
-from crud.app_crud_api import app_crud_api
+from crudy.app_crud import app_crud
+from crudy.app_crud_api import app_crud_api
 app.register_blueprint(app_crud)
 app.register_blueprint(app_crud_api)
 
